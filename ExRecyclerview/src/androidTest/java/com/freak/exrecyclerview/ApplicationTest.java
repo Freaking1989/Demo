677fefbbe2322ba721e97f432eb997f13ec0b7c5
freaking1989 <651478191@qq.com>
2017-03-28 16:59:19 +0800

    去掉多余包..

---
+++ ExRecyclerview/src/androidTest/java/com/freak/exrecyclerview/ApplicationTest.java
@@ -0,0 +1,22 @@
+package com.freak.exrecyclerview;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
